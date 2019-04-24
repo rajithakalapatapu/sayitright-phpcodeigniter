2ee173649a04790109ed1f6f137dfd62a66afa62
rajitha <rajithakalapatapu9@gmail.com>
2019-04-23 23:12:17 -0500

    Routes and header link fixed

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,14 +14,14 @@
 
 <nav>
     <div class="nav_left">
-        <a href="HomePage.php">
+        <a href="<?php echo base_url(); ?>index.php/home">
             <img src="<?php echo base_url(); ?>imgsay/logo.png">
         </a>
     </div>
     <div class="nav_right">
         <ul>
             <li><a href="<?php echo base_url(); ?>index.php/home">Home</a></li>
-            <li><a href="<?php echo base_url(); ?>index.php/about" class="activetab">About US</a></li>
+            <li><a href="<?php echo base_url(); ?>index.php/about">About US</a></li>
             <li><a href="#">Blog</a></li>
             <li><a href="<?php echo base_url(); ?>index.php/buyfromus">Buy From US</a></li>
             <li><a href="<?php echo base_url(); ?>index.php/contactus">Contact US</a></li>
