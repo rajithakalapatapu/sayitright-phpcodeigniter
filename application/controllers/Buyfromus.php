ad2ea235ff14dbf784603bdf6b4dacd1d773b9d1
rajitha <rajithakalapatapu9@gmail.com>
2019-04-24 20:12:12 -0500

    Buyfromus

---
+++ application/controllers/Buyfromus.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Buyfromus extends CI_Controller
+{
+    public function index()
+    {
+
+        $this->load->helper('url');
+
+        $data['title'] = ucfirst(get_class($this)); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/' . strtolower(get_class($this)) .'.php');
+        $this->load->view('templates/footer', $data);
+
+    }
+
+}
+
