ba6e33b665c0094bc0304d540b9208782ede8b78
rajitha <rajithakalapatapu9@gmail.com>
2019-04-24 19:54:24 -0500

    create separate controller for about

---
+++ application/controllers/About.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class About extends CI_Controller
+{
+    public function index()
+    {
+
+        $this->load->helper('url');
+
+        $data['title'] = ucfirst(get_class($this)); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/about.php');
+        $this->load->view('templates/footer', $data);
+
+    }
+
+}
