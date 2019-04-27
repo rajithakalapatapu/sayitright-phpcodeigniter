65aa138366e92d19a14f40e578c930fdafac4d30
rajitha <rajithakalapatapu9@gmail.com>
2019-04-27 09:56:16 -0500

    remove php extension from redirection

--- application/views/pages/redirect_to_loginpage.php
+++ application/views/pages/redirect_to_loginpage.php
@@ -1,10 +1,10 @@
 <?php
 if ($this->session->user_type == "individual") {
-    echo '<script>window.location.href = "individuallogin.php";</script>';
+    echo '<script>window.location.href = "individuallogin";</script>';
 } else if ($this->session->user_type == "event") {
-    echo '<script>window.location.href = "eventlogin.php";</script>';
+    echo '<script>window.location.href = "eventlogin";</script>';
 } else if ($this->session->user_type == "business") {
-    echo '<script>window.location.href = "businesslogin.php";</script>';
+    echo '<script>window.location.href = "businesslogin";</script>';
 } else {
-    echo '<script>window.location.href = "login.php";</script>';
+    echo '<script>window.location.href = "login";</script>';
 }
