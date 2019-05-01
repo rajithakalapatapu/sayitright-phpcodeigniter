ed0173986e1067894c55ff151da79d933858bf7f
rajitha <rajithakalapatapu9@gmail.com>
2019-04-30 21:53:25 -0500

    use sql syntax for getting products

--- application/models/Buyfromus_model.php
+++ application/models/Buyfromus_model.php
@@ -11,8 +11,26 @@ class Buyfromus_model extends CI_Model
 
     public function get_products()
     {
-        $query = $this->db->get('products');
-        return $query->result_array();
+        $stmt = "select * from products;";
+        $sql = sprintf($stmt);
 
+        $result = $this->db->query($sql);
+
+        $all_products = array();
+
+        foreach ($result->result() as $row) {
+            $product = array(
+                'product_name' => $row->product_name,
+                'product_description' => $row->product_description,
+                'product_picture' => $row->product_picture,
+                'price_per_unit' => $row->price_per_unit,
+                'product_id' => $row->product_id,
+                'user_id' => $this->session->user_id
+
+            );
+            array_push($all_products, $product);
+        }
+
+        return $all_products;
     }
 }
